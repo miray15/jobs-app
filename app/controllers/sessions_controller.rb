ed802fba71668ebe33207c27efdedc888ddc3cb9
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-02 12:28:31 -0500

    Revert "add full stack authentication"
    This reverts commit 858b13965b097ef0212dbd7d24fa6eb32726b625.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,23 @@
 class SessionsController < ApplicationController
 
 
-    def new
-      render template: "sessions/new"
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+    else
+      render json: {}, status: :unauthorized
     end
-   
-    def create
-      user = User.find_by(email: params[:email])
-      if user && user.authenticate(params[:password])
-        session[:user_id] = user.id
-        redirect_to "/"
-      else
-        redirect_to "/login"
-      end
-    end
-   
-    def destroy
-      session[:user_id] = nil
-      redirect_to "/login"
-    end
+  end
 
+  
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,8 @@ Rails.application.routes.draw do
 
   resources :will_applies
 
-  get "/signup" => "users#new"
   post "/users" => "users#create"
 
-  get "/login" => "sessions#new"
-  get "/logout" => "sessions#destroy"
   post "/sessions" => "sessions#create"
-
   
-
 end
-
