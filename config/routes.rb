5a9173aebed844d3a8a997590421d7aec67bf319
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-02 11:35:43 -0500

    add users create action (sign up)

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   resources :will_applies
 
+  post "/users" => "users#create"
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_162136) do
-  # These are extiesnsions that must be enabled in order to support this database
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_162933) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "titles", force: :cascade do |t|
@@ -25,6 +25,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_02_162136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "will_applies", force: :cascade do |t|
