858b13965b097ef0212dbd7d24fa6eb32726b625
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-02 12:13:45 -0500

    add full stack authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
 
   resources :will_applies
 
+  get "/signup" => "users#new"
   post "/users" => "users#create"
 
+  get "/login" => "sessions#new"
+  get "/logout" => "sessions#destroy"
   post "/sessions" => "sessions#create"
+
   
+
 end
+
