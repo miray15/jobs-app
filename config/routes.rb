a8f04fcc21c1943a31fb9c9441e8fc5882e78368
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-02 15:14:03 -0500

    add authentication form and additional files

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   resources :titles
 
-# get "/titles" => "titles#index"
 
   resources :will_applies
 
   resources :users
 
-  resources :sessions
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
   
   
 end 
