ed268aa13c0225cbe1038cc8921139b937b5d7f4
Miray Karabulut <mimikarabulut@gmail.com>
2023-05-03 22:31:34 -0500

    update searches controller logic in index action

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,37 +4,69 @@ require 'net/http'
 
 class SearchesController < ApplicationController
 
-
-
-def index 
-  url = URI("https://jsearch.p.rapidapi.com/search?query=#{params[:search]}&page=1&num_pages=1")
-  
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  
-  request = Net::HTTP::Get.new(url)
-  request["X-RapidAPI-Key"] = 'Rails.application.credentials.jobs_api_key'
-  request["X-RapidAPI-Host"] = 'jsearch.p.rapidapi.com'
-  
-  response = http.request(request)
-  render :index
-
-    @searches = Search.all
+  def index 
+    url = URI("https://jsearch.p.rapidapi.com/search?query=#{params[:search]}&page=1&num_pages=1")
     
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = Rails.application.credentials.jobs_api_key
+    request["X-RapidAPI-Host"] = 'jsearch.p.rapidapi.com'
+    
+    response = http.request(request)
+  
+    @searches = Search.all
+      
     if params[:search_by_title] && params[:search_by_title] != ""
       @searches = @searches.where("title like ?", 
-      "%# {params[:search_by_title]}%")
+        "%#{params[:search_by_title]}%")
     end
+    
     if params[:search_by_location] && params[:search_by_location] != ""
       @searches = @searches.where("location like ?", 
-      "%# {params[:search_by_location]}%")
+        "%#{params[:search_by_location]}%")
     end
-   if params[:search_by_salary] && params[:search_by_salary] != ""
+    
+    if params[:search_by_salary] && params[:search_by_salary] != ""
       @searches = @searches.where("salary like ?", 
-      "%# {params[:search_by_salary]}%")
+        "%#{params[:search_by_salary]}%")
     end
-end 
+    
+    render :index
+  end 
+  end 
   
 
-end
+# def index 
+#   url = URI("https://jsearch.p.rapidapi.com/search?query=#{params[:search]}&page=1&num_pages=1")
+  
+#   http = Net::HTTP.new(url.host, url.port)
+#   http.use_ssl = true
+  
+#   request = Net::HTTP::Get.new(url)
+#   request["X-RapidAPI-Key"] = 'Rails.application.credentials.jobs_api_key'
+#   request["X-RapidAPI-Host"] = 'jsearch.p.rapidapi.com'
+  
+#   response = http.request(request)
+#   render :index
+
+#     @searches = Search.all
+    
+#     if params[:search_by_title] && params[:search_by_title] != ""
+#       @searches = @searches.where("title like ?", 
+#       "%# {params[:search_by_title]}%")
+#     end
+#     if params[:search_by_location] && params[:search_by_location] != ""
+#       @searches = @searches.where("location like ?", 
+#       "%# {params[:search_by_location]}%")
+#     end
+#    if params[:search_by_salary] && params[:search_by_salary] != ""
+#       @searches = @searches.where("salary like ?", 
+#       "%# {params[:search_by_salary]}%")
+#     end
+# end 
+  
+
+# end
 
